ad35396d6651cb4bd8df888c2d6d4eb119468ddc
bmoreto <beatriz_moreto@hotmail.com>
2025-05-25 14:57:41 -0300

    update

--- enviar.php
+++ enviar.php
@@ -26,7 +26,7 @@ $body_content = "De: $nome \n E-mail: $email \n Telefone: $telefone \n Mensagem:
 
 if ($_POST['leaveblank'] != '' or $_POST['dontchange'] != 'http://') {
 
-  echo "<h2
+	echo "<h2
 	style=\"
 	font-size: 1em;
 	margin-top: 20%;
@@ -35,41 +35,39 @@ if ($_POST['leaveblank'] != '' or $_POST['dontchange'] != 'http://') {
 	font-weight: normal;
 	color: #1b1b1b;
 	\"><center><span>Aconteceu algum erro!</span><p>Você pode tentar denovo ou enviar direto para " . $email_envio . "!</p></center><h2>";
-	
+
 	echo "<html style=\"background: #fff;\"></html>";
 	echo "<meta HTTP-EQUIV='Refresh' CONTENT='10;URL=" . $site_url . "'>";
-}
+} else if (isset($_POST['nome'])) {
 
-else if (isset($_POST['nome'])){
+	require('./PHPMailer/PHPMailerAutoload.php');
 
-require ('./PHPMailer/PHPMailerAutoload.php');
+	$mail = new PHPMailer;
+	$mail->CharSet = 'UTF-8';
 
-$mail = new PHPMailer;
-$mail->CharSet = 'UTF-8';
+	$mail->isSMTP();
+	$mail->Host = $host_smtp;
+	$mail->SMTPAuth = true;
+	$mail->Username = $email_envio;
+	$mail->Password = $email_pass;
+	$mail->Port = $host_port;
 
-$mail->isSMTP();
-$mail->Host = $host_smtp;
-$mail->SMTPAuth = true;
-$mail->Username = $email_envio;
-$mail->Password = $email_pass;
-$mail->Port = $host_port; 
+	$mail->From = $email_envio;
 
-$mail->From = $email_envio;
+	$mail->addAddress($email_envio);
 
-$mail->addAddress($email_envio);
+	$mail->FromName = 'Formulário de Contato';
+	$mail->AddReplyTo($_POST['email'], $_POST['nome']);
 
-$mail->FromName = 'Formulário de Contato';
-$mail->AddReplyTo($_POST['email'], $_POST['nome']);
+	$mail->WordWrap = 70;
 
-$mail->WordWrap = 70;
+	$mail->Subject = 'Formulário - ' . $site_name . ' - ' . $_POST['nome'];
 
-$mail->Subject = 'Formulário - ' . $site_name . ' - ' . $_POST['nome'];
+	$mail->Body = $body_content;
 
-$mail->Body = $body_content;
+	if (!$mail->send()) {
 
-if(!$mail->send()) {
-  
-  echo "<h2
+		echo "<h2
 	style=\"
 	font-size: 1.5em;
 	margin-top: 20%;
@@ -78,13 +76,12 @@ if(!$mail->send()) {
 	font-weight: normal;
 	color: #fdc64b;
 	\"><center><span>Aconteceu algum erro!</span><p>Você pode tentar denovo ou enviar direto para " . $email_envio . "!</p></center><h2>";
-	
-	echo "<html style=\"background: #fff;\"></html>";
-	echo "<meta HTTP-EQUIV='Refresh' CONTENT='10;URL=" . $site_url . "'>";
-  
-} else {
 
-  echo "<h2
+		echo "<html style=\"background: #fff;\"></html>";
+		echo "<meta HTTP-EQUIV='Refresh' CONTENT='10;URL=" . $site_url . "'>";
+	} else {
+
+		echo "<h2
 	style=\"
 	font-size: 1.5em;
 	margin-top: 20%;
@@ -93,9 +90,8 @@ if(!$mail->send()) {
 	font-weight: normal;
 	color: #89bb50;
 	\"><center><span>Formulário Enviado</span><p>Em breve eu entro em contato com você. Abraços.</p></center><h2>";
-	
-	echo "<html style=\"background: #fff;\"></html>";
-	echo "<meta HTTP-EQUIV='Refresh' CONTENT='2;URL=" . $site_url . "'>";
+
+		echo "<html style=\"background: #fff;\"></html>";
+		echo "<meta HTTP-EQUIV='Refresh' CONTENT='2;URL=" . $site_url . "'>";
+	}
 }
-}
-?>
